aeb6916e8526159b85d787fe4da37ce062d83245
sanjay <sgupta@enbake.com>
2013-12-10 14:41:30 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Palacart::Application.config.secret_key_base = '7076b764da474867e88e7a994d01db497309da29620f68ca417f2186bb389ba4a007cedcda653b30697a9b0755349430f4803b5ee4cf9403e6d9a7f993740684'
